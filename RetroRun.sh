ede6f9fd7af4e58de4c6e28e372ca1eee0856f32
FollyMaddy <meulenf@gmx.net>
2019-11-29 21:34:42 +0100

    added info about resolution function

--- RetroRun.sh
+++ RetroRun.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # Program : RetroRun.sh
-# Version : 1.3
+# Version : 1.4
 
 # Use : 
-# RetroRun is a simple program created to run allmost all ROMS of RetroPie from your windows desktop enviroment (X).
+# RetroRun is a simple program created to run allmost all ROMS of RetroPie from your windows desktop enviroment (X) with pcmanfm filemanager
+# If a resolution is added to the script, then roms will run in an alternative resolution
+# If you quit the rom, the computer turns back in the original resolution
+
 # Dependencies : 
 # RetroRun only works if RetroPie is installed on your computer.
 # RetroRun uses pcmanfm filemanager !!!
@@ -12,8 +15,10 @@
 # How to run :
 # An install script and an uninstall script is added
 # Discription of what these scripts do are in the scripts themselves.
-# Make the program executable, dubbleclick and choose open in terminal.
+# Make the install or uninstall script executable, dubbleclick and choose open in terminal.
 # Or run it from the terminal with : ./install.sh
+# If you want to start RetroRun in a different resolution then  run the installer from the terminal with, for example : ./install.sh 800x600
+# You can check your available resolutions with : xrandr
 # for uninstall use the file uninstall.sh
 
 # Additional tips :
