80b1613d060d8d1e0212e3ab3ceda46ff4aef16b
FollyMaddy <meulenf@gmx.net>
2020-02-12 17:36:38 +0100

    Add files via upload

--- RetroRun_onstart.sh
+++ RetroRun_onstart.sh
@@ -1,6 +1,21 @@
 #before using this addon-script, install feh with :
 #sudo apt-get install feh
 
-#uncomment the "feh" line to display a black image before and after executing a rom
+#uncomment one "feh" line to display an image before and after executing a rom
 #(so the windows-screen is not visable)
+
+#uncomment one of these images !
+# black carbon_fiber image
 #feh -F -N -Z -Y -q /etc/emulationstation/themes/carbon/art/carbon_fiber.png &
+# or
+# Joystick image
+#feh -F -N -Z -Y -q $HOME/ RetroPie/retropiemenu/icons/rpsetup.png &
+# or
+# RetroPie image
+#feh -F -N -Z -Y -q $HOME/ RetroPie/retropiemenu/icons/splashscreen.png &
+# or
+# Retroarch image
+#feh -F -N -Z -Y -q $HOME/RetroPie/retropiemenu/icons/retroarch.png &
+# or
+# Runcommand image
+#feh -F -N -Z -Y -q $HOME/ RetroPie/retropiemenu/icons/runcommand.png &
